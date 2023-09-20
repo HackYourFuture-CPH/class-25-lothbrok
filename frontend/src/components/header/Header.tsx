1720ec7e9cf2f6acf949d144dcfa5ec46f92e3d4
Arash <arash8877@gmail.com>
2023-09-20 21:09:39 +0200

    added elements of header and make it responsible

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
-import './Header.css';
-import SearchBar from '../searchBar/SearchBar';
+import React from "react";
+import "./Header.css";
+import SearchBar from "../searchBar/SearchBar";
+import PageTitle from "../pageTitle/PageTitle";
 
 const Header = () => {
   return (
-    <div>
-        <SearchBar/>
+    <div className="header container">
+      <PageTitle />
+      <button className="burger-icon-btn">
+        <img className="image-Arash" src="/assets/icons/burger.png"/>
+      </button>
+      <div className="logo">
+        <div className="logo-icon-div">
+          <img className="image-Arash" src="/assets/icons/tablet-icon.png"/>
+        </div>
+        <h3>Dashboard</h3>
+      </div>
+      <div className="header-right">
+        <SearchBar />
+        <div className="notification-icon-div">
+          <img className="image-Arash"  src="/assets/icons/notification.png"/>
+        </div>
+        <div className="img-account-div">
+          <img className="image-Arash" src="/assets/images/account.png" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
---
+++ frontend/src/components/pageTitle/PageTitle.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './PageTitle.css'
+
+const PageTitle = () => {
+  return (
+    <h2>Dashboard</h2>
+  )
+}
+
+export default PageTitle;
