9c9855be35aa2265fa73470e9988b0e4a6748ebe
Arash <arash8877@gmail.com>
2023-09-26 21:08:46 +0200

    added header for projectDetails

---
+++ frontend/src/components/projectDetailsHeader/ProjectDetailsHeader.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import './ProjectDetailsHeader.css'
+import { Button, Menu, MenuItem, IconButton } from "@mui/material/";
+import { MoreHoriz, CloseRounded } from "@mui/icons-material/";
+
+const ProjectDetailsHeader = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div className="project-details-header">
+      <IconButton aria-label="close">
+        <CloseRounded />
+      </IconButton>
+      <div>
+        <Button
+          id="basic-button"
+          aria-controls={open ? "basic-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          onClick={handleClick}
+        >
+          <MoreHoriz />
+        </Button>
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": "basic-button",
+          }}
+        >
+          <MenuItem onClick={handleClose}>option 1</MenuItem>
+          <MenuItem onClick={handleClose}>option 2</MenuItem>
+          <MenuItem onClick={handleClose}>option 3</MenuItem>
+        </Menu>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectDetailsHeader;
