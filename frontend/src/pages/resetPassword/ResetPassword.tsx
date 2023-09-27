e5c8799416eeaed0bc56e4f34895d6adcadb8c16
Yurii Pakhariev <paharevy@gmail.com>
2023-09-27 22:06:40 +0200

    Added CQ configurations

--- frontend/src/pages/resetPassword/ResetPassword.tsx
+++ frontend/src/pages/resetPassword/ResetPassword.tsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from "react";
-import ForgotPasswordLayout from "../../components/ForgotPasswordLayout";
-import { Button, TextField, InputLabel } from "@mui/material";
-import { useNavigate } from "react-router-dom";
-import { auth } from "../../firebase_config";
-import { confirmPasswordReset } from "firebase/auth";
-import "./resetPassword.css";
-import { useForm, SubmitHandler } from "react-hook-form";
-import { passwordPattern } from "../../passwordPattern";
+import React, { useState, useEffect } from 'react';
+import ForgotPasswordLayout from '../../components/ForgotPasswordLayout';
+import { Button, TextField, InputLabel } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { auth } from '../../firebase_config';
+import { confirmPasswordReset } from 'firebase/auth';
+import './resetPassword.css';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { passwordPattern } from '../../passwordPattern';
 
 interface FormData {
   password: string;
@@ -17,23 +17,23 @@ const ResetPassword = () => {
   const {
     register,
     handleSubmit,
-    formState: { errors, isValid },
+    formState: { isValid },
     getValues,
-  } = useForm<FormData>({ mode: "all" });
+  } = useForm<FormData>({ mode: 'all' });
 
-  const [oobCode, setOobCode] = useState("");
+  const [oobCode, setOobCode] = useState('');
   const navigate = useNavigate();
 
   useEffect(() => {
     const currentUrl = window.location.href;
     const urlParams: any = new URLSearchParams(new URL(currentUrl).search);
-    setOobCode(urlParams.get("oobCode"));
+    setOobCode(urlParams.get('oobCode'));
   }, []);
 
   const onSubmit: SubmitHandler<FormData> = async (data) => {
     try {
       await confirmPasswordReset(auth, oobCode, data.password);
-      navigate("/login");
+      navigate('/login');
     } catch (e) {
       console.error(e);
     }
@@ -42,51 +42,47 @@ const ResetPassword = () => {
   const passwordPatternValidator = (value: string) => {
     return (
       passwordPattern.test(value) ||
-      "Password must contain at least one uppercase letter and at least one symbol (! or _)"
+      'Password must contain at least one uppercase letter and at least one symbol (! or _)'
     );
   };
 
   const passwordMatchValidator = () => {
     const { password, confirmPassword } = getValues();
-    return password === confirmPassword || "Passwords do not match";
+    return password === confirmPassword || 'Passwords do not match';
   };
 
   return (
     <ForgotPasswordLayout>
       <p>To change your password, please fill in the fields below</p>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <InputLabel htmlFor="pass" style={{ color: "#55555F" }}>
+        <InputLabel htmlFor='pass' style={{ color: '#55555F' }}>
           New Password
         </InputLabel>
         <TextField
-          className="input-styles"
-          type="password"
-          {...register("password", {
-            required: "Password is required",
+          className='input-styles'
+          type='password'
+          {...register('password', {
+            required: 'Password is required',
             validate: {
               pattern: passwordPatternValidator,
             },
           })}
         />
-        <InputLabel htmlFor="confirm-pass" style={{ color: '#55555F' }}>
+        <InputLabel htmlFor='confirm-pass' style={{ color: '#55555F' }}>
           Confirm New Password
         </InputLabel>
         <TextField
-          className="input-styles"
-          type="password"
-          {...register("confirmPassword", {
-            required: "Please confirm your password",
+          className='input-styles'
+          type='password'
+          {...register('confirmPassword', {
+            required: 'Please confirm your password',
             validate: {
               pattern: passwordPatternValidator,
               matches: passwordMatchValidator,
             },
           })}
         />
-        <Button
-          type="submit"
-          variant="contained"
-          disabled={!isValid}
-        >
+        <Button type='submit' variant='contained' disabled={!isValid}>
           Submit
         </Button>
       </form>
