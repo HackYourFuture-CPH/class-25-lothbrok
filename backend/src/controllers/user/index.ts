1ca72278f943121ab9b35bd3d0f65aa4b66e96c7
Darya-s <darya.stepanova95@gmail.com>
2023-10-02 11:37:14 +0200

    Changes after feedback, using node library for HTTP status code, separated functions for user update in DB and in Firebase

--- backend/src/controllers/user/index.ts
+++ backend/src/controllers/user/index.ts
@@ -13,22 +13,35 @@ export const getUserById = async (req: Request, res: Response) => {
   }
 };
 
-export const updateUserById = async (req: Request, res: Response) => {
+export const updateProfileInFirebase = async (req: Request, res: Response) => {
   try {
     const uid = req.params.uid;
 
-    const { first_name, last_name, photourl, phone_number } = req.body;
+    const { first_name, last_name, photourl, phone_number, email } = req.body;
     const user = await adminFireAuth.updateUser(uid, {
       displayName: `${first_name},${last_name}`,
       photoURL: photourl,
       phoneNumber: phone_number,
+      email: email,
     });
 
+    res.status(200).send({ user });
+  } catch (error) {
+    res.status(404);
+  }
+};
+
+export const updateUserByIdInDB = async (req: Request, res: Response) => {
+  try {
+    const uid = req.params.uid;
+
+    const { first_name, last_name, photourl, phone_number } = req.body;
+
     const userInfo = await db('users')
-      .where('id', uid)
+      .where('uid', uid)
       .update(first_name, last_name, photourl, phone_number);
 
-    res.status(200).send({ user, userInfo });
+    res.status(200).send({ userInfo });
   } catch (error) {
     res.status(404);
   }
--- backend/src/routes/user/index.ts
+++ backend/src/routes/user/index.ts
@@ -1,10 +1,15 @@
-import { registerUserToDb, updateUserById, getUserById } from '../../controllers/user';
+import {
+  registerUserToDb,
+  updateUserByIdInDB,
+  getUserById,
+  updateProfileInFirebase,
+} from '../../controllers/user';
 import express, { Router } from 'express';
 
 const userRoute: Router = express.Router();
 
 userRoute.get('/current', getUserById);
-userRoute.put('/update', updateUserById);
+userRoute.put('/update', updateUserByIdInDB, updateProfileInFirebase);
 userRoute.post('/register', registerUserToDb);
 
 export default userRoute;
